a0c64d9149ec878e81fb5d1ca0fe6789f86f6af3
atom-at-335292957824 <summer.hasani@gmail.com>
2017-09-07 17:02:13 +0100

    initial commit

---
+++ src/main/java/com/summ3r/howdycloudy/HowdyCloudyApplication.java
@@ -0,0 +1,12 @@
+package com.summ3r.howdycloudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowdyCloudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowdyCloudyApplication.class, args);
+	}
+}
